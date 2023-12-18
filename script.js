2d8bde913f11f6a7c4559f17ee147cba18c90144
dev24 <dev24@macmini-dev24.local>
2023-12-18 15:43:54 +0300

    pochti finish

--- script.js
+++ script.js
@@ -28,13 +28,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // Табы на главной
+    // Табы на главной проекты
 
     let tabs = document.querySelectorAll('.mainProjects__tablinks');
     
-    if (tabs) {
-        document.getElementById("tab1").classList.add('active');
-        document.querySelector('[data-tab=tab1]').classList.add("active");
+    if (tabs.length !== 0) {
+        document.getElementById("tab1")?.classList.add('active');
+        document.querySelector('[data-tab=tab1]')?.classList.add("active");
 
         tabs.forEach(function (tab) {
             tab.addEventListener('click', function () {
@@ -62,4 +62,139 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelector('[data-tab="' + tabName + '"]').classList.add("active");
         }
     }
+
+    // Табы на главной реализованные проекты
+
+    let rtabs = document.querySelectorAll('.mainRealised__tablinks');
+    
+    if (rtabs.length !== 0) {
+        document.getElementById("rtab1")?.classList.add('active');
+        document.querySelector('[data-re-tab=rtab1]')?.classList.add("active");
+
+        rtabs.forEach(function (tab) {
+            tab.addEventListener('click', function () {
+                var tabName = this.getAttribute('data-re-tab');
+                openTab(tabName);
+            });
+        });
+        
+        function openTab(tabName) {
+            let i, tabcontent, tablinks;
+        
+            // Скрываем все содержимое вкладок
+            tabcontent = document.getElementsByClassName("mainRealised__tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].classList.remove('active')
+            }
+        
+            // Деактивируем все кнопки вкладок
+            rtabs.forEach(function (tab) {
+                tab.classList.remove("active");
+            });
+        
+            // Показываем содержимое выбранной вкладки и активируем соответствующую кнопку
+            document.getElementById(tabName).classList.add('active');
+            document.querySelector('[data-re-tab="' + tabName + '"]').classList.add("active");
+        }
+    }
+
+    // Слайдер наши проекты
+
+    const swiperProject = new Swiper('.swiperProject', {
+        direction: 'horizontal',
+        spaceBetween: 20,
+        loop: true,
+        
+        pagination: {
+          el: '.swiper-pagination-project',
+          clickable: true,
+        },
+    });
+    
+    const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+    if (bullets.length !== 0) {
+        bullets.forEach(el => {
+            el.addEventListener('mouseenter', (e) => {
+                e.target.click();
+            });
+        })
+    }
+
+    // Слайдер реализованные проекты
+
+
+    // const swiperRealised = new Swiper('.swiperRealised', {
+    //     direction: 'horizontal',
+    //     slidesPerView: 3,
+    //     spaceBetween: 36,
+    //     loop: true,
+            
+    //     navigation: {
+    //         nextEl: '.swiper-realised-next',
+    //         prevEl: '.swiper-realised-prev',
+    //     },
+    //  });
+
+    const arrow_next = document.querySelectorAll('.mainRealised__next');
+    const arrow_prev = document.querySelectorAll('.mainRealised__prev');
+
+    // console.log(arrow_next)
+
+    if (arrow_next.length !== 0) {
+        arrow_next.forEach(arr => {
+            arr.addEventListener('click', (e) => {
+                let list = e.target.parentNode.firstElementChild;
+                list.scrollBy(350, 0);
+            });
+        });
+    }
+
+    if (arrow_prev.length !== 0) {
+        arrow_prev.forEach(arr => {
+            arr.addEventListener('click', (e) => {
+                let list = e.target.parentNode.firstElementChild;
+                list.scrollBy(-350, 0);
+            });
+        });
+    }
+        
+    // Ссылка на все проекты, перенос вниз
+
+    const elem1 = document.querySelector('.mainProjects__panel-link');
+    const place1 = document.querySelector('.mainProjects__inner');
+    const back1 = document.querySelector('.mainProjects__panel');
+    
+    if (window.innerWidth < 577) {
+        place1.append(elem1);
+    } else {
+        back1.append(elem1);
+    }
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth < 577) {
+            place1.append(elem1);
+        } else {
+            back1.append(elem1);
+        }
+    });
+
+    // Ссылка на все реализованные проекты, перенос вниз
+
+    const elem2 = document.querySelector('.mainRealised__panel-link');
+    const place2 = document.querySelector('.mainRealised__inner');
+    const back2 = document.querySelector('.mainRealised__panel');
+    
+    if (window.innerWidth < 577) {
+        place2.append(elem2);
+    } else {
+        back2.append(elem2);
+    }
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth < 577) {
+            place2.append(elem2);
+        } else {
+            back2.append(elem2);
+        }
+    });
 });
